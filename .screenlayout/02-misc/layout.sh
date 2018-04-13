dda94f6bf1d55f45c6ca528af99dd03b2ed6f976
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2018-04-13 15:30:02 +0200

    Added screen layouts and gitignore

---
+++ .screenlayout/02-misc/layout.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --dpi 192 --mode 2560x1440 --pos 2880x1200 --rotate normal --output DP1 --off --output DP2-1 --off --output HDMI1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --auto --primary --right-of eDP1 --output HDMI1 --off --output DP2 --off
+xrandr --dpi 192
