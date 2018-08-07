c7345d0331f7478859e0d4762aaa5e30e3915c3d
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2018-08-07 20:21:09 +0200

    eDP1 is now turned off when dock is connected

--- .screenlayout/01-home/layout.sh
+++ .screenlayout/01-home/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 2880x0 --rotate normal --output DP1 --off --output DP2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.5x1.5 --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.5x1.5 --output DP2-1 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
