1f569e62da1e287a63ed2678c9ad1aac2bd0029d
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2018-09-06 09:34:18 +0200

    Turned eDP1 off

--- .screenlayout/02-misc/layout.sh
+++ .screenlayout/02-misc/layout.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --dpi 192 --mode 2560x1440 --pos 2880x1200 --rotate normal --output DP1 --off --output DP2-1 --off --output HDMI1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --auto --primary --right-of eDP1 --output HDMI1 --off --output DP2 --off
-xrandr --dpi 192
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
