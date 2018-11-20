548a1a9857e6d6e1393b64cd03c26c7136d39c03
Mistadon <mail@moritzwm.de>
2018-11-20 14:58:08 +0100

    Added custom dwm PKGBUILD for automatic patching. Added systray patch

--- dwm/config.h
+++ dwm/config.h
@@ -22,10 +22,12 @@ static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 8;       /* snap pixel */
 static const unsigned int gappx     = 0;        /* gap between windows */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int showsystray        = 0;        /* 1 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, 0: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -43,7 +45,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -72,7 +74,9 @@ static const char *vol_down[] = { "pactl", "set-sink-volume", "0", "-5%", NULL }
 static const char *vol_up[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *vol_toggle[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *brightness_down[] = { "xbacklight", "-dec", "2", NULL };
+static const char *brightness_ten_down[] = { "xbacklight", "-dec", "10", NULL };
 static const char *brightness_up[] = { "xbacklight", "-inc", "2", NULL };
+static const char *brightness_ten_up[] = { "xbacklight", "-inc", "10", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -105,7 +109,9 @@ static Key keys[] = {
 	{ 0,                            XF86AudioLowerVolume,      spawn, {.v = vol_down } },
 	{ 0,                            XF86AudioMute,             spawn, {.v = vol_toggle } },
 	{ 0,                            XF86MonBrightnessUp,       spawn, {.v = brightness_up } },
+	{ ShiftMask,                    XF86MonBrightnessUp,       spawn, {.v = brightness_ten_up } },
 	{ 0,                            XF86MonBrightnessDown,     spawn, {.v = brightness_down } },
+	{ ShiftMask,                    XF86MonBrightnessDown,     spawn, {.v = brightness_ten_down } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
