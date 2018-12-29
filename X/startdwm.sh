7ba06a164359baede1735a5aecdec5e9d2b2557b
Mistadon <mail@moritzwm.de>
2018-12-29 10:45:49 +0100

    Removed the checksum code, wasn't necessary

--- X/startdwm.sh
+++ X/startdwm.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-# relaunch DWM if the binary changes, otherwise bail
-# also, log errors to a file
-csum=$(sha1sum $(which dwm))
-new_csum=""
+# start DWM and log errors to a file
 while true
 do
-    if [ "$csum" != "$new_csum" ]
-    then
-        csum=$new_csum
-        dwm 2> ~/.dwm.log
-    else
-        exit 0
-    fi
-    new_csum=$(sha1sum $(which dwm))
-    sleep 1
+    dwm 2> ~/.dwm.log
 done
