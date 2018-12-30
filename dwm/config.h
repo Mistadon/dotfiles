147874a43ecaf4d313158d4bc573e4402c782015
Mistadon <mail@moritzwm.de>
2018-12-30 16:03:17 +0100

    Externalised surf launch command

--- dwm/config.h
+++ dwm/config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *duckduckgocmd[] = { "surf", "https://start.duckduckgo.com", NULL };
+static const char *duckduckgocmd[] = { "surf", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
