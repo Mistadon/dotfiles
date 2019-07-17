207887c134d9028cd067e410bb096b52a9bdc1cd
Mistadon <mail@moritzwm.de>
2019-07-17 14:57:15 +0200

    Removed compton, added stalonetray

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# Keyring
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+export SSH_AUTH_SOCK
+
 # Autostart applications
-compton -b # Compositor
+stalonetray & # Systray
 udiskie -s & # Udiskie, automounts thumb drives
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & # Graphical authentication agent
 nextcloud & # Nextcloud
 nm-applet & # Networkmanager
 caffeine & # Caffeine
+
+# Kill systray, it's annoying and can be started on demand. Only needs to start so I don't have a nextcloud popup
+sleep 2 && pkill stalonetray &
