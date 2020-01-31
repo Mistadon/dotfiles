922d71d800fe2099ca4964da6a7b45c99b9c5afe
MoritzWM <mail@moritzwm.de>
2020-01-31 14:39:25 +0100

    Removed unnecessary stuff

--- scripts/set_status.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ "$2" == "temporary" ]; then
-    date +%s > /run/user/$(id -u)/tmp_status
-fi
-xsetroot -name " $1 "
--- scripts/set_volume.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pactl set-sink-volume 0 $1
-date +%s > /run/user/$(id -u)/tmp_status
-sh ~/git/dotfiles/scripts/set_status.sh "Set volume to $(pactl list sinks | grep -m 1 'Volume: ' | awk '{print $5}')"
